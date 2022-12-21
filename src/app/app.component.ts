da7af8968a36aac9a704a7d4b39dd5613edc19c6
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-12-21 20:30:53 -0300

    Contador app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Contador App';
+  numero: number = 10;
+  numbase: number = 5;
+
+  acumular( valor: number ) {
+    this.numero += valor;
+  }
 }

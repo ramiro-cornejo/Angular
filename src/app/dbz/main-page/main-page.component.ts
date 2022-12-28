54e60e87c4fc482ab71f4642bfc0db5f9851b0bf
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-12-28 11:20:59 -0300

    FormsModule / ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 15000
+  }
+
+  agregarForm() {
+    console.log('Prueba de funcionamiento');
+  }
 }
